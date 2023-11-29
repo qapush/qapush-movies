089c5b146c3e1039baeac0fa0022c9806fa7c8be
Michael Kapush <mihail.kapush@gmail.com>
2023-11-29 22:41:22 +0100

    Styles

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import styles from './MovieCard.module.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styles from "./MovieCard.module.css";
+import { Link } from "react-router-dom";
 
 export default function MovieCard({ movie }) {
+  const { genres, poster, title, year, id } = movie;
+  return (
+    <Link to={`movie/${id}`} className={styles.movieCard}>
+      <div>
+        <img src={poster} alt={title} />
+        <div className={styles.description}>
+          <h2>{title}</h2>
+          <span className={styles.year}>{year}</span>
+          <div className={styles.genres}>{generateGenres(genres)}</div>
+        </div>
+      </div>
+    </Link>
+  );
+}
 
-    const { genres, poster, title, year, id } = movie;
+const generateGenres = (arr) => {
+  return arr.map((i) => {
     return (
-        <Link to={`movie/${id}`} className={styles.movieCard}>
-            <div>
-                <img src={poster} alt={title} />
-                <h2>{title}</h2>
-                <hr />
-                <span>{year}</span>
-                <div className={styles.genres}>
-                    {generateGenres(genres)}
-                </div>
-            </div>
-        </Link>
-    )
-}
-
-const generateGenres = arr => {
-    return arr.map(i => {
-        return <span className={styles.genre} key={i.id}>{i.name}</span>
-    })
-}
+      <span className={styles.genre} key={i.id}>
+        {i.name}
+      </span>
+    );
+  });
+};
