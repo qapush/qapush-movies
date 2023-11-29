17d92e2a62ba2718e740d8f6c43fb9fb00391599
qapush <mihail.kapush@gmail.com>
2023-11-29 18:20:57 +0100

    Start recommendation page

---
+++ src/components/Recommend/Recommend.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Recommend.module.css';
+
+export default function Recommend() {
+  return <div>Recommend</div>;
+}
--- src/index.js
+++ src/index.js
@@ -3,34 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import MoviePage from './components/MoviePage/MoviePage';
-import { store } from './store'
-import { Provider } from 'react-redux'
+import { store } from './store';
+import { Provider } from 'react-redux';
 import Wrapper from './components/Wrapper/Wrapper';
 import {
   createBrowserRouter,
   RouterProvider,
   Navigate,
   createRoutesFromElements,
-  Route
-} from "react-router-dom";
+  Route,
+} from 'react-router-dom';
+import Recommend from './components/Recommend/Recommend';
 
 const router = createBrowserRouter(
-  
   createRoutesFromElements(
     <Route path="/" element={<Wrapper />}>
-      <Route path="/" element={<App/>} />
+      <Route path="/" element={<App />} />
+      <Route path="/recommend" element={<Recommend />} />
       <Route path="/movie/:movieId" element={<MoviePage />} />
       <Route path="*" element={<Navigate to="/" />} />
-    </Route>
-  )
+    </Route>,
+  ),
 );
 
 const rootElement = document.getElementById('root');
 
-
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <Provider store={store}>
-      <RouterProvider router={router} />
-  </Provider>
+    <RouterProvider router={router} />
+  </Provider>,
 );
