1d39b349f85bfe5c80ecaba7fb01d47420ce0088
Michael Kapush <mihail.kapush@gmail.com>
2023-12-05 16:08:36 +0100

    Recommend link

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -2,6 +2,9 @@ import { Outlet } from 'react-router-dom';
 import { useEffect } from 'react';
 import { setThemeAuto } from '../../utils/theme';
 import Header from '../Header/Header';
+import { Link } from 'react-router-dom';
+import styles from './Wrapper.module.css';
+
 
 
 export default function Wrapper() {
@@ -12,9 +15,12 @@ export default function Wrapper() {
   }, [])
 
   return (
-    <div id='wrapper'>
+    <div className={styles.wrapper}>
         <Header />
         <Outlet/>
+        <div className={styles.footer}>
+          <Link to="/recommend">recommend movie or serial</Link>
+        </div>
     </div>
   )
 }
